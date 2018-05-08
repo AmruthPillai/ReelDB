02c4afbc97257b77cc9b3ff4c0b631b6796b049d
AmruthPillai <im.amruth@gmail.com>
2018-05-08 17:23:04 +0530

    enable cache

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var app = angular.module('reelDb', []);
 app.controller('MoviesController', function ($scope, $http) {
     $scope.akshaya = 'Akshaya';
     
-    $http.get('https://api.myjson.com/bins/17k0ay')
+    $http.get('https://api.myjson.com/bins/17k0ay', { cache: true })
         .then(function (res) {
             $scope.movies = res.data;
         });
