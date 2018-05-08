a596837700f5a534d9bb870e111ce69a09fe6522
AmruthPillai <im.amruth@gmail.com>
2018-05-08 15:36:54 +0530

    UI changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var app = angular.module('reelDb', []);
 
 app.controller('MoviesController', function ($scope, $http) {
+    $scope.akshaya = 'Akshaya';
+    
     $http.get('https://api.myjson.com/bins/17k0ay')
         .then(function (res) {
             $scope.movies = res.data;
