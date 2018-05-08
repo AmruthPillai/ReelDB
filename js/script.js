deb184300d2f588a2e2a512f959d3e1419efb5b2
AmruthPillai <im.amruth@gmail.com>
2018-05-08 17:27:51 +0530

    added local DB, instead of jsonbin API

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var app = angular.module('reelDb', []);
 app.controller('MoviesController', function ($scope, $http) {
     $scope.akshaya = 'Akshaya';
     
-    $http.get('https://api.myjson.com/bins/17k0ay', { cache: true })
+    $http.get('../assets/movies.json', { cache: true })
         .then(function (res) {
             $scope.movies = res.data;
         });
