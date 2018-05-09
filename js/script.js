9431729cc78dd0a7d78eff6bd8e491dbf5e62e4c
Amruth Pillai <im.amruth@gmail.com>
2018-05-09 08:43:54 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var app = angular.module('reelDb', []);
 app.controller('MoviesController', function ($scope, $http) {
     $scope.akshaya = 'Akshaya';
     
-    $http.get('../assets/movies.json', { cache: true })
+    $http.get('https://amruthpillai.github.io/ReelDB/assets/movies.json', { cache: true })
         .then(function (res) {
             $scope.movies = res.data;
         });
